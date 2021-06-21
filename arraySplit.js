2c22aa196a77faa0a300455c2aab32f3524b9f15
quentincrida <qcrida@gmail.com>
2021-06-21 13:01:55 +0100

    Both tests passing simultaneously

--- arraySplit.js
+++ arraySplit.js
@@ -1,28 +1,21 @@
-
 'use strict';
 
 // 1. given an array of length >=0
 //2. Return the contents of the array divided into N sized arrays
 //3. Where the remainder can't be divided by n, final array should be equal in length to the remainder
 
-// const arr1 = [1, 2, 3, 4, 5, 6, 7];
-// const arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// const arr3 = [10, 12, 15];
-const result = [];
-
 //declare a function that takes 2 arguments: array and size
- const arraySplit = function (arr, size) {
+const arraySplit = function(arr, size) {
+  //declare an empty array to store the nested arrays
+  const result = [];
   //loop through the array (arr)
-  for(let i = 0; i < arr.length; i += size){
-    //push the result to a new empty array, using slice() method to create sub arrays
-  result.push(arr.slice(i, i + size));
+  for (let i = 0; i < arr.length; i += size) {
+    //push the result to a new empty array, using slice() method to create nested arrays
+    result.push(arr.slice(i, i + size));
   }
-  //print the result (an array of arrays) to the console to see that it is working correctly
+  //return the new array of sub arrays
   return result;
-  // console.log(result);
 }
-//call the function using the two parameters arr, and size of the sub arrays
-// arraySplit(arr2, 8);
-// console.log(arr2.length);
+
 
 module.exports = arraySplit;
--- arraySplit_spec.js
+++ arraySplit_spec.js
@@ -2,20 +2,32 @@ const assert = require('assert');
 const arraySplit = require('./arraySplit.js');
 
 
-describe('arraySplit', function () {
+describe('arraySplit', function() {
   let arr;
 
-  beforeEach(function () {
+
+  beforeEach(function() {
     arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   });
 
-  it('should split the array into even sub arrays of 3', function () {
-        assert.deepStrictEqual(arraySplit(arr, 3), [[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
+  it('should split the array into even sub arrays of 3', function() {
+    const actual = arraySplit(arr, 3);
+    assert.deepStrictEqual(actual, [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9]
+    ]);
   });
 
-  xit('should enable the final sub array to be shorter', function () {
-    assert.deepStrictEqual(arraySplit(arr, 2), [[1,2],[3, 4], [5,6], [7,8], [9]]);
-  })
-
+  it('should enable the final sub array to be shorter', function() {
+    actual = arraySplit(arr, 2);
+    assert.deepStrictEqual(actual, [
+      [1, 2],
+      [3, 4],
+      [5, 6],
+      [7, 8],
+      [9]
+    ]);
+  });
 
 });
